b6179af72e0e39fa223bbffff3ad0352e6045d39
augoodma <augoodma@asu.edu>
2020-08-24 08:56:13 -0400

    initial

---
+++ cmake-build-debug/CMakeFiles/Module2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Module2.dir/GoodmanCourseReader.c.o"
+  "Module2"
+  "Module2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Module2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
